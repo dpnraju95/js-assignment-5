149bb9e7a12342a72e6caaef968af51ccf592f69
dpnraju95 <dpnraju95@gmail.com>
2021-11-30 11:24:56 +0530

    final chages

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,14 @@
 function CreateObject(arr) {
     // Write your code here
+
+    dit= {}
+
+    for(let i=0;i<arr.length;i=i+2){
+        dit[arr[i]] = arr[i+1]
+    }
+
+    return dit
+
 }
 
 module.exports = CreateObject;
